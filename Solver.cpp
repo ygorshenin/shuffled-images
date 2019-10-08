319be518faaaae97f1f0b0d0aec5a28577ae40c0
Yuri Gorshenin <mipt.vi002@gmail.com>
2019-10-08 23:45:43 +0300

    First so-so working version of Solver.

--- Solver.cpp
+++ Solver.cpp
@@ -4,9 +4,19 @@
 #include <iomanip>
 #include <iostream>
 #include <numeric>
+#include <random>
 #include <utility>
 #include <vector>
 using namespace std;
+
+enum class Where { Up, Down, Left, Right };
+const int kNumDirs = 4;
+const int kDRow[] = {-1, +1, 0, 0};
+const int kDCol[] = {0, 0, -1, +1};
+const Where kDirs[] = {Where::Up, Where::Down, Where::Left, Where::Right};
+const int kFreeCell = -1;
+
+mt19937 g_engine{42};
 
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T>& vs) {
@@ -32,6 +42,12 @@ template <typename T>
 void SortUnique(vector<T>& vs) {
   sort(vs.begin(), vs.end());
   vs.erase(unique(vs.begin(), vs.end()), vs.end());
+}
+
+template <typename T>
+void RemoveAll(vector<T>& vs, const T& v) {
+  auto it = remove(vs.begin(), vs.end(), v);
+  vs.erase(it, vs.end());
 }
 
 template <typename T>
@@ -79,12 +95,38 @@ double GetScore(int size, const vector<int>& expected, const vector<int>& actual
 }
 
 struct Piece {
+  Piece() = default;
+  Piece(int row, int col, int piece)
+    : m_row(row)
+    , m_col(col)
+    , m_piece(piece) {
+  }
+
   int m_row = 0;
   int m_col = 0;
   int m_piece = 0;
 };
 
+struct Cell {
+  Cell() = default;
+  Cell(int row, int col)
+    : m_row(row)
+    , m_col(col) {
+  }
+
+  int m_row = 0;
+  int m_col = 0;
+};
+
 struct BoundingBox {
+  BoundingBox() = default;
+  BoundingBox(int minRow, int minCol, int maxRow, int maxCol)
+    : m_minRow(minRow)
+    , m_minCol(minCol)
+    , m_maxRow(maxRow)
+    , m_maxCol(maxCol) {
+  }
+
   int NumRows() const {
     assert(m_minRow <= m_maxRow);
     return m_maxRow - m_minRow;
@@ -95,16 +137,177 @@ struct BoundingBox {
     return m_maxCol - m_minCol;
   }
 
-  int m_minRow = 0;
-  int m_minCol = 0;
-  int m_maxRow = 0;
-  int m_maxCol = 0;
+  void AddInplace(const Cell& cell) {
+    m_minRow = min(m_minRow, cell.m_row);
+    m_minCol = min(m_minCol, cell.m_col);
+    m_maxRow = max(m_maxRow, cell.m_row + 1);
+    m_maxCol = max(m_maxCol, cell.m_col + 1);
+  }
+
+  [[nodiscard]] BoundingBox Add(const Cell& cell) const {
+    auto bb = *this;
+    bb.AddInplace(cell);
+    return bb;
+  }
+
+  int m_minRow = numeric_limits<int>::max();
+  int m_minCol = numeric_limits<int>::max();
+  int m_maxRow = numeric_limits<int>::min();
+  int m_maxCol = numeric_limits<int>::min();
 };
 
+bool IsValid(const vector<vector<int>>& field, int r, int c) {
+  if (r < 0 || r >= field.size())
+    return false;
+  if (c < 0 || c >= field[r].size())
+    return false;
+  return true;
+}
+
+double GetPredict(int p, int q, Where where, const Context& ctx) {
+  switch (where) {
+    case Where::Left:
+      return ctx.m_hpreds[p][q];
+    case Where::Right:
+      return ctx.m_hpreds[q][p];
+    case Where::Up:
+      return ctx.m_vpreds[p][q];
+    case Where::Down:
+      return ctx.m_vpreds[q][p];
+  }
+}
+
+template <typename TFn>
+void ForEachCandidateCell(const vector<vector<int>>& field, const BoundingBox& bbox, const Context& ctx, TFn&& fn) {
+  assert(bbox.NumRows() <= ctx.m_size);
+  assert(bbox.NumCols() <= ctx.m_size);
+
+  for (int row = 0; row < field.size(); ++row) {
+    for (int col = 0; col < field[0].size(); ++col) {
+      if (field[row][col] != kFreeCell)
+        continue;
+      const auto bb = bbox.Add(Cell(row, col));
+      if (bb.NumRows() > ctx.m_size || bb.NumCols() > ctx.m_size)
+        continue;
+
+      bool good = false;
+      for (int k = 0; k < kNumDirs && !good; ++k) {
+        const auto r = row + kDRow[k];
+        const auto c = col + kDCol[k];
+
+        if (IsValid(field, r, c))
+          good = good || field[r][c] != kFreeCell;
+      }
+      if (good)
+        fn(Cell(row, col));
+    }
+  }
+}
+
+struct Solution {
+  Solution() = default;
+
+  template <typename P>
+  Solution(P&& permutation, double score)
+    : m_permutation(std::forward<P>(permutation))
+    , m_score(score) {
+  }
+
+  vector<int> m_permutation;
+  double m_score = 0;
+};
+
+Solution HillClimbing(const Context& ctx) {
+  if (ctx.m_total == 0)
+    return {};
+
+  vector<vector<int>> field(2 * ctx.m_size, vector<int>(2 * ctx.m_size, kFreeCell));
+  BoundingBox bbox;
+  field[ctx.m_size][ctx.m_size] = 0;
+
+  vector<int> avail(ctx.m_total);
+  iota(avail.begin(), avail.end(), 0);
+
+  shuffle(avail.begin(), avail.end(), g_engine);
+
+  auto markUsed = [&](const Cell& cell, int p) {
+    field[cell.m_row][cell.m_col] = p;
+    bbox.AddInplace(cell);
+    RemoveAll(avail, p);
+  };
+
+  markUsed(Cell(ctx.m_size, ctx.m_size), avail[0]);
+  double totalScore = 0;
+  int totalEdges = 0;
+
+  for (int i = 1; i < ctx.m_total; ++i) {
+    Cell bestCell;
+    double bestScore = -1;
+    int bestEdges = 1;
+    int bestP = -1;
+
+    vector<Cell> cells;
+    ForEachCandidateCell(field, bbox, ctx, [&](const Cell& cell) { cells.push_back(cell); });
+    shuffle(cells.begin(), cells.end(), g_engine);
+
+    assert(!cells.empty());
+    assert(!avail.empty());
+    for (const auto& cell : cells) {
+      for (const auto& p : avail) {
+        assert(field[cell.m_row][cell.m_col] == kFreeCell);
+        double score = 0;
+        int edges = 0;
+        for (int k = 0; k < kNumDirs; ++k) {
+          const auto r = cell.m_row + kDRow[k];
+          const auto c = cell.m_col + kDCol[k];
+          if (IsValid(field, r, c) && field[r][c] != kFreeCell) {
+            const auto q = field[r][c];
+            score += GetPredict(q, p, kDirs[k], ctx);
+            ++edges;
+          }
+        }
+
+        if (bestScore < 0 ||
+            (totalScore + bestScore) / (totalEdges + bestEdges) < (totalScore + score) / (totalEdges + edges)) {
+          bestScore = score;
+          bestEdges = edges;
+          bestCell = cell;
+          bestP = p;
+        }
+      }
+    }
+    assert(bestScore >= 0);
+    assert(bestP >= 0);
+
+    markUsed(bestCell, bestP);
+
+    totalScore += bestScore;
+    totalEdges += bestEdges;
+  }
+
+  assert(avail.empty());
+  assert(bbox.NumRows() == ctx.m_size);
+  assert(bbox.NumCols() == ctx.m_size);
+
+  vector<int> result;
+  for (int r = bbox.m_minRow; r < bbox.m_maxRow; ++r) {
+    for (int c = bbox.m_minCol; c < bbox.m_maxCol; ++c) {
+      result.push_back(field[r][c]);
+    }
+  }
+
+  return Solution{std::move(result), totalScore / totalEdges};
+}
+
 vector<int> Solve(const Context& ctx) {
-  vector<int> result(ctx.m_total);
-  iota(result.begin(), result.end(), 0);
-  return result;
+  Solution best = HillClimbing(ctx);
+  for (int i = 0; i < 10000; ++i) {
+    const auto curr = HillClimbing(ctx);
+    if (curr.m_score > best.m_score)
+      best = curr;
+  }
+  cerr << "Best solution score: " << best.m_score << endl;
+  return best.m_permutation;
 }
 
 int main() {
